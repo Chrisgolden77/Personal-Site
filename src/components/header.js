dc14b31bb6795db4329e86a0b48d4de41b4fb752
Chris Golden <chrgolden@gmail.com>
2020-04-28 11:51:11 -0700

    removes console log in header

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const assignClass = route => {
-  console.log(`ROUTE = ${route}`)
-
   return window.location.pathname === route
     ? 'header-link active-route'
     : 'header-link';
