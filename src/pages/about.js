5012f615c7128b760a0b0794a307502c47c678a7
Chris Golden <chrgolden@gmail.com>
2020-04-28 18:58:18 -0700

    some typo correction

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,10 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const AboutPage = ({path}) => (
+const AboutPage = ({ path }) => (
   <Layout path={path}>
     <SEO title="About" />
-    <h1>Hi from the AboutPage page</h1>
+    <h1>Hi from the About Page</h1>
     <p>Welcome to the About page</p>
   </Layout>
 );
