c0c6e5f90cce6826cec13d25f3f25310ef29180b
Chris Golden <chrgolden@gmail.com>
2020-04-30 09:32:43 -0700

    changes mobile menu to use hooks for displaying links

--- src/components/mobileHeader.js
+++ src/components/mobileHeader.js
@@ -1,37 +1,45 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../images/chris-logo.png';
 
 const MobileHeader = ({ siteTitle, path }) => {
+  let [isMobileMenuOpen, setMobileMenuState] = useState(false);
+
   const assignClass = link => {
     return path === link ? 'header-link active-link' : 'header-link';
   };
-  const displayLinks = () => {};
+
   return (
     <header id="mobile-header">
       <h1 className="mobile-home-link">
-        <img src={Logo} alt="Chris Logo" className="header-logo" />
+        <img
+          src={Logo}
+          alt="Chris Logo"
+          className="header-logo"
+          onClick={() => setMobileMenuState(!isMobileMenuOpen)}
+        />
       </h1>
-
-      <div id="header-links">
-        <Link className={assignClass('/')} to="/">
-          Home
-        </Link>
-        <Link className={assignClass('/about/')} to="/about/">
-          About
-        </Link>
-        <Link className={assignClass('/apps/')} to="/apps/">
-          My Apps
-        </Link>
-        <Link className={assignClass('/resume/')} to="/resume/">
-          Resume
-        </Link>
-        <Link className={assignClass('/contact/')} to="/contact/">
-          Contact
-        </Link>
-      </div>
+      {isMobileMenuOpen ? (
+        <div id="header-links">
+          <Link className={assignClass('/')} to="/">
+            Home
+          </Link>
+          <Link className={assignClass('/about/')} to="/about/">
+            About
+          </Link>
+          <Link className={assignClass('/apps/')} to="/apps/">
+            My Apps
+          </Link>
+          <Link className={assignClass('/resume/')} to="/resume/">
+            Resume
+          </Link>
+          <Link className={assignClass('/contact/')} to="/contact/">
+            Contact
+          </Link>
+        </div>
+      ) : null}
     </header>
   );
 };
