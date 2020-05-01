cdeee124f41ecfdaa19c1e1fd84d8c116937c539
Chris Golden <chrgolden@gmail.com>
2020-05-01 16:05:52 -0700

    renames resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,17 +6,13 @@ import SEO from '../components/seo';
 const ResumePage = ({ path }) => (
   <Layout path={path}>
     <SEO title="Resume " />
-    {/* <div className="mobile-resume-title">
-      <h1>
-        My Resume
-      </h1>
-    </div> */}
       <iframe
-        className="resumeIframe"
+        className="resume-iframe"
         src="https://chrisgolden77.github.io/Resume/Chris_Golden_Resume.pdf"
         title="resume"
         scrolling='no'
       />
+      
   </Layout>
 );
 
