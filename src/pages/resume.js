26179a8954e73abbb2e363e32f277b05d0d07989
Chris Golden <chrgolden@gmail.com>
2020-05-04 09:44:31 -0700

    testing pdf loader

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Document, Page} from 'react-pdf';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -6,18 +7,23 @@ import SEO from '../components/seo';
 const ResumePage = ({ path }) => (
   <Layout path={path}>
     <SEO title="Resume " />
-    {/* <div className="mobile-resume-title">
-      <h1>
-        My Resume
-      </h1>
-    </div> */}
-    <embed
+    <div>
+      <Document
+      className="resume-iframe"
+        file="https://chrisgolden77.github.io/Resume/Chris_Golden_Resume.pdf"
+        >
+          <Page pageNumber={1} /> 
+        </Document>
+
+    </div>
+    
+    {/* <embed
       alt="Chris GOlden's Resume"
       className="resume-iframe"
       src="https://chrisgolden77.github.io/Resume/Chris_Golden_Resume.pdf"
       title="resume"
       scrolling="no"
-    ></embed>
+    ></embed> */}
   </Layout>
 );
 
