44c0ba3122badefaa6574d0c2ea65889a6a1d4de
Chris Golden <chrgolden@gmail.com>
2020-05-04 13:33:59 -0700

    adds contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,14 +3,41 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const ContactPage = ({path}) => (
+const ContactPage = ({ path }) => (
   <Layout path={path}>
-    <SEO title="Contact Me"/>
-    <div  className="mobile-page-title">
-
-    </div>
+    <SEO title="Contact Me" />
+    <div className="mobile-page-title"></div>
     <h1>Want to get ahold of me?</h1>
     <p>Drop me a line through email! </p>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{' '}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     {/* <Link to="/">Go back to the homepage</Link> */}
   </Layout>
 );
