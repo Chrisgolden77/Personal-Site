ee4132a1a382fcb2c3fd8dc65c91f865f5b1861e
Chris Golden <chrgolden@gmail.com>
2020-05-04 10:27:58 -0700

    testing mobile pdf viewer

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import {Document, Page} from 'react-pdf';
-
+import { MobilePDFReader} from 'react-read-pdf';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const ResumePage = ({ path }) => (
   <Layout path={path}>
     <SEO title="Resume " />
-    <div>
-      <Document
-      className="resume-iframe"
-        file="https://chrisgolden77.github.io/Resume/Chris_Golden_Resume.pdf"
-        >
-          <Page pageNumber={1} /> 
-        </Document>
-
-    </div>
-    
-    {/* <embed
+    <embed
       alt="Chris GOlden's Resume"
       className="resume-iframe"
       src="https://chrisgolden77.github.io/Resume/Chris_Golden_Resume.pdf"
       title="resume"
       scrolling="no"
-    ></embed> */}
+    ></embed>
+    <div id="mobile-pdf-viewer">
+      <MobilePDFReader
+        page={1}
+        id="mobile-pdf-view"
+        width="100vw"
+        height="1000px"
+        showAllPage
+        // className="resume-iframe"
+        url={"https://chrisgolden77.github.io/Resume/Chris_Golden_Resume.pdf"}
+      />
+    </div>
   </Layout>
 );
 
