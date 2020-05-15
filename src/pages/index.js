04203e87c97137fdac39d59367d2120f73be701c
Chris Golden <chrgolden@gmail.com>
2020-05-15 15:26:49 -0700

    some more changes to front page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,32 +10,33 @@ const IndexPage = ({ path }) => (
   <Layout className="home" path={path}>
     <SEO title="Home" />
     {/* <div className="mobile-page-title"></div> */}
-    <div id="home-hero">
-      <img
-        alt="Photo of Chris"
-        id="personal-picture"
-        src="https://media-exp1.licdn.com/dms/image/C5603AQHsMjURLrwZfg/profile-displayphoto-shrink_200_200/0?e=1593648000&v=beta&t=OQ__yff_40-knIDiqERm-oZv3Sx2VZ1Lwz-MuLQEqeE"
-      />
-      <div id="home-hero-paragraph">
-        <h1>
-          I'm
-          <em> Chris Golden </em>
-          <br />
-          Welcome to my personal site!
-        </h1>
-        <h3>
-          I'm a full stack developer with a passion for learning, problem solving, and collaborating.
-          
-          Take a look around, drop me a line, or take a peek at the code with
-          the github link below.
-        </h3>
-        <div style={{ fontSize: '75px' }}>
-          {/* <FontAwesomeIcon width="30px" icon={faHandPointDown} /> */}
+    <div id="home-hero-wrapper">
+      <div id="home-hero">
+        <img
+          alt="Photo of Chris"
+          id="personal-picture"
+          src="https://media-exp1.licdn.com/dms/image/C5603AQHsMjURLrwZfg/profile-displayphoto-shrink_200_200/0?e=1593648000&v=beta&t=OQ__yff_40-knIDiqERm-oZv3Sx2VZ1Lwz-MuLQEqeE"
+        />
+        <div id="home-hero-paragraph">
+          <h1>
+            I'm
+            <em> Chris Golden </em>
+            <br />
+            Welcome to my personal site!
+          </h1>
+          <h3>
+            I'm a full stack developer with a passion for learning, problem
+            solving, and collaborating. Take a look around, drop me a line, or
+            take a peek at the code with the github link below.
+          </h3>
+          <div style={{ fontSize: '75px' }}>
+            {/* <FontAwesomeIcon width="30px" icon={faHandPointDown} /> */}
+          </div>
+          <h4 float="bottom">
+            Keep scrolling for personal stats.
+            <br />
+          </h4>
         </div>
-        <h4 float="bottom">
-          Keep scrolling for personal stats.
-          <br />
-        </h4>
       </div>
     </div>
     <div id="home-content">
