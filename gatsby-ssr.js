2f6982279fdd0b7cc404bbe605af645fb42d81a8
Chris Golden <chrgolden@gmail.com>
2020-06-23 11:23:35 -0700

    adds script to head in ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,5 +3,15 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
+const React = require("react");
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+      <script
+        type="text/javascript"
+        src="https://dev-app.swarmreport.org/widget.js"
+        name="bee-widget"
+        companyuid="1001"
+      ></script>
+  ]);
+};
 // You can delete this file if you're not using it
