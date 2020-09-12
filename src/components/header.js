92ff80a9fd63332ff97f2bd3cad79cb02e5c79cd
Chris Golden <chrgolden@gmail.com>
2020-09-12 14:00:49 -0700

    adds useffect to trigger rerender for selected menu item

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Logo from '../images/chris-logo.png';
 
 const Header = ({ menuLinks, path, siteTitle }) => {
+  const [classNamesReady, setClassNamesReady] = useState(false);
+  useEffect(() => setClassNamesReady(true), []);
   const assignClass = (link, isLogo) => {
     let className = isLogo ? 'home-link' : 'header-link';
     if (path === link && !isLogo) className += ' active-link';
@@ -29,10 +31,7 @@ const Header = ({ menuLinks, path, siteTitle }) => {
   return (
     <header id="header">
       <div id="home-link-wrapper">
-        <Link
-          className={assignClass('/', true) || 'home-link active-home-link'}
-          to="/"
-        >
+        <Link className={assignClass('/', true)} to="/">
           <img alt="header logo" className="header-logo" src={Logo} />
         </Link>
       </div>
