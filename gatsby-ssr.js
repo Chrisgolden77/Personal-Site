b322a07dcfdd2fa735640bca07d9f05168bd920a
Chris Golden <chrgolden@gmail.com>
2020-11-06 16:03:08 -0800

    updates script for bee app

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,16 +3,14 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-const React = require("react");
-exports.onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-      <script
-        type="text/javascript"
-        src="https://app-qa.swarmreport.org/widget.js"
-        id="bee-widget"
-        data-company-uid="00000000-0000-0000-0000-000000000000"
-        data-environment="qa"
-      ></script>
+const React = require('react');
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <script
+      type="text/javascript"
+      src="https://client-dev/api/widget/?companyUid=00000000-0000-0000-0000-000000000000"
+      id="bee-widget"
+    ></script>,
   ]);
 };
 // You can delete this file if you're not using it
