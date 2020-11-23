ea19624bafa37364b55af248d75fa3dea4145b73
Chris Golden <chrgolden@gmail.com>
2020-11-23 00:08:35 -0800

    changes some style

--- src/components/appCard.js
+++ src/components/appCard.js
@@ -8,7 +8,7 @@ const AppCard = ({ app, onClick }) => {
         tabIndex="0"
         onClick={() => onClick()}
         onKeyDown={() => onClick()}
-        style={{ height: '100%', overflowY: "scroll" }}
+        style={{ height: '100%', overflowY: "auto" }}
       >
         <h2>{app.appName}</h2>
         <img
