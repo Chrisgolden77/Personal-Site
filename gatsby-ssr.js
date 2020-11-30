70504dc816dfe8e60d73870ac782ccf51047a276
Chris Golden <chrgolden@gmail.com>
2020-11-30 12:58:50 -0800

    updates ssr to load script in postBodyContents instead of preBodyContents

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,8 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 const React = require('react');
-exports.onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
     <script
       type="text/javascript"
       src="https://client-dev.swarmreport.org/api/widget/?companyUid=00000000-0000-0000-0000-000000000000"
