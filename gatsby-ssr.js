12e62c55f94b385727c8e9d6448ba4c76c0f443a
Chris Golden <chrgolden@gmail.com>
2020-11-30 22:31:15 -0800

    adds script to Helmet until we fix swarmz

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,17 @@
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-const React = require('react');
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      type="text/javascript"
-      src="https://client-dev.swarmreport.org/api/widget/?companyUid=00000000-0000-0000-0000-000000000000"
-      id="bee-widget"
-    />,
-  ]);
-};
-// You can delete this file if you're not using it
+//  * See: https://www.gatsbyjs.org/docs/ssr-apis/
+//  */
+// const React = require('react');
+// const beeWidgetScriptElement = (
+//   <script
+//     type="text/javascript"
+//     src="https://client-dev.swarmreport.org/api/widget/?companyUid=00000000-0000-0000-0000-000000000000"
+//     id="bee-widget"
+//   />
+// );
+// exports.replaceRenderer = ({ setPostBodyComponents }) => {
+//   setPostBodyComponents([beeWidgetScriptElement]);
+// };
+// // You can delete this file if you're not using it
