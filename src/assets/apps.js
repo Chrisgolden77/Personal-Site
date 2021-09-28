213ded16281210b91c4c7ec882fd6b7444c4cfc9
Chris Golden <37640545+Chrisgolden77@users.noreply.github.com>
2021-09-28 08:53:50 -0700

    updates apps and languages

--- src/assets/apps.js
+++ src/assets/apps.js
@@ -1,5 +1,13 @@
 const Apps = [
   {
+    appImages: [require('../images/L6-phones.png'), require('../images/L6-Desktop.png'), require('../images/L6-desktop-map.png'),require('../images/L6-mobile-maps.png'),require('../images/L6-Appointment-summary.png'), require('../images/L6-modal-alert.png')],
+    appName: 'CU-APPS Lobby 6 Scheduling ',
+    appDescription:
+    'The Lobby6 Scheduling app is an appointment scheduling web app for for clients of Credit Unions.\n\nI worked creating reusable styled components, creating laravel api endpoints, improving UI/UX visuals, and dynamic sizing for mobile.',
+    appLink: 'http://www.cu-apps.com/',
+    appLinkText: 'More Info about CU-APPs',
+  },
+  {
     appImages: [require('../images/donut-chart.png'), require('../images/performance-chart.png'), require('../images/waterfall.png'),require('../images/asin-breakdown.png'), require('../images/data-select.png')],
     appName: 'SCOUT by RUN AMZ',
     appDescription:
